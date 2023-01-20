a1ba48a5d82c52d3913e05b3370b75cbdef28af9
firsttoken <monikasharma12330@gmail.com>
2023-01-20 10:05:28 +0530

    Bubble_sort

---
+++ Bubble_sort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Concept
+{
+    public class Bubble_sort
+    {
+        public Bubble_sort()
+        {
+            int[] arr = { 34, 65, 4, 66, 37 };
+            int temp;
+            for(int j=0;j<=arr.Length-2;j++)
+            {
+                for(int i=0;i<=arr.Length-2;i++)
+                {
+                    if (arr[i]>arr[i+1] )
+                    {
+                        temp=arr[i + 1];
+                        arr[i+1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Array is sorted by bubble sort:");
+            foreach(int ele in arr)
+            {
+                Console.WriteLine(ele);
+            }
+        
+        }
+    }
+}
