7789732312f1003c4793289e0d12d6af73daee93
firsttoken <monikasharma12330@gmail.com>
2023-01-20 10:02:29 +0530

    check string is anagram

---
+++ Anagram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Concept
+{
+    public class Anagram
+    {
+        public void CheckAnagram()
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+            char[] chararr1=str1.ToLower().ToCharArray();
+            char[] chararr2=str2.ToLower().ToCharArray();
+            Array.Sort(chararr1);
+            Array.Sort(chararr2);
+            string newarray1=new string(chararr1);
+            string newarray2=new string(chararr2);
+
+            if(newarray1==newarray2)
+            {
+                Console.WriteLine("Strings are Anagrams.");
+            }
+            else
+            {
+                Console.WriteLine("Strings are not Anagrams.");
+            }
+        }
+    }
+}
