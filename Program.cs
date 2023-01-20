cc80e7c7defbb25635d71157b6160adde33cad21
firsttoken <monikasharma12330@gmail.com>
2023-01-20 10:01:04 +0530

    main file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sorting_Concept
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            // Bubble_sort bs=new Bubble_sort();
+            //  Insertion_Sort isobj=new Insertion_Sort();
+           // Anagram anagram= new Anagram();
+            //anagram.CheckAnagram();
+            BinarySearch binarySearch=new BinarySearch();
+            int index=binarySearch.FindElementByBinarySearch();
+            Console.WriteLine("The element is present on index: " + index);   
+        }
+    }
+}
